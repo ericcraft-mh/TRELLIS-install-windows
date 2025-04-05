5c5f81a52a164c75ccb019b10146791767400547
ericcraft-mh <99364056+ericcraft-mh@users.noreply.github.com>
2025-04-05 16:17:45 -0500

    Add files via upload
    TRELLIS-install.bat: TRELLIS install batch mirroring code in README.md.
    requirements-torch.txt: First requirements file to be installed due to dependencies. Place in TRELLIS folder before install.
    requirements-other.txt: Second requirements file to be installed due to dependencies on pytorch. Place in TRELLIS folder before install.

---
+++ TRELLIS-install.bat
@@ -0,0 +1,29 @@
+@echo off
+
+cd /d "%~dp0"
+cd ..
+echo *** The current directory is: %cd%
+
+echo *** Cloning TRELLIS repository
+git clone --recurse-submodules https://github.com/microsoft/TRELLIS.git
+cd TRELLIS
+
+echo *** Creating venv
+py -3.10 -m venv "trellis-venv"
+call trellis-venv\scripts\activate.bat
+
+echo *** Upgrading pip, wheel, setuptools
+python -m pip install --upgrade pip wheel
+python -m pip install setuptools==75.8.2
+
+echo *** Installing requirements
+pip install -r requirements-torch.txt
+pip install -r requirements-other.txt
+
+echo *** Set Environment Variables
+set ATTN_BACKEND=flash-attn
+@REM set ATTN_BACKEND=xformers
+set SPCONV_ALGO=native
+set XFORMERS_FORCE_DISABLE_TRITON=1
+
+python ./app.py
